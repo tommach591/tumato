c5a88939359783f21f4cd865a822aa2314ae4411
Tom Mach <57111473+tommach591@users.noreply.github.com>
2023-07-31 17:42:29 -0700

    decided on tailwind

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,13 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Fishdex from "../../assets/fishdex.json";
+import Login from "../Login";
 
 function App() {
-  const fishdex: Object = JSON.parse(JSON.stringify(Fishdex));
-
   return (
     <div className="App">
-      {Object.values(fishdex).map((entry, i) => {
-        return <img key={i} src={entry.image} alt="" />;
-      })}
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
     </div>
   );
 }
