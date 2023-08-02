5f7730fb228e13de5fbf338828315ae2555622fc
Tom Mach <57111473+tommach591@users.noreply.github.com>
2023-08-02 16:45:20 -0700

    Update Login.tsx

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,62 @@
+import { useState } from "react";
+
 function Login() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    console.log(`Log in ${username} ${password}`);
+  };
+
+  const handleSignup = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    console.log(`Register ${username} ${password}`);
+  };
+
+  return (
+    <div className="flex w-screen h-screen justify-center">
+      <div className="flex flex-col item-center w-80 h-52 my-12 border border-solid border-black rounded shadow-md">
+        <h1 className="w-full text-center mt-4 mb-2 text-xl font-semibold">
+          Login
+        </h1>
+        <div className="flex flex-col items-center w-full">
+          <label className="w-10/12 m-1 text-right">
+            Username:
+            <input
+              type="text"
+              value={username}
+              onChange={(event) => setUsername(event.currentTarget.value)}
+              className="border border-solid border-black rounded ml-1"
+            />
+          </label>
+          <label className="w-10/12 m-1 text-right">
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={(event) => setPassword(event.currentTarget.value)}
+              className="border border-solid border-black rounded ml-1"
+            />
+          </label>
+          <div className="flex justify-center w-full mt-4">
+            <button
+              onClick={(event) => handleLogin(event)}
+              className="border border-solid border-black rounded m-2 pl-2 pr-2"
+            >
+              Log In
+            </button>
+            <button
+              onClick={(event) => handleSignup(event)}
+              className="border border-solid border-black rounded m-2 pl-2 pr-2"
+            >
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
